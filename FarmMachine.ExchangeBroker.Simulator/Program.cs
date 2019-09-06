38842f23a17361167081356c94829015cf2eb33a
Winster332 <stas-martynov-98@mail.ru>
2019-09-06 13:06:08 +0300

    created project FarmMachine.ExchangeBroker.Simulator

---
+++ FarmMachine.ExchangeBroker.Simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FarmMachine.ExchangeBroker.Simulator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
